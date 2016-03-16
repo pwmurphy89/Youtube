081c284598ec735d3b6857cee7f36dd24905e03e
curtjenk <curtjenk@comcast.net>
2016-03-16 14:19:55 -0400

    Added modal.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // $('.square-pop-up').on('click', 'div>img', showModal); 
+    $('.img-launch-modal').click(updateModal);
+});
+
+function updateModal() {
+ /*
+  Do something here to update the modal contents!
+
+ */
+}
